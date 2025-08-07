10e21d48eb1556d375aa26bf777d9331ff87a895
magnificodev <richardnguyen26th8@gmail.com>
2025-08-07 15:03:11 +0700

    add docker-compose file to the project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+const API_URL = import.meta.VITE_API_BASE_URL || "/api"
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
@@ -12,11 +14,13 @@ export default defineConfig({
         },
     },
     server: {
-        proxy: {
+        host: "0.0.0.0",
+        port: 3000,
+        proxy: API_URL.startsWith("/api") ? {
             "/api": {
                 target: "http://localhost:8000",
                 changeOrigin: true,
             },
-        },
+        } : undefined,
     },
 })
