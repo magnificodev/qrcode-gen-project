0a06f1a82f2a392ece2cd0e8172ec3f34a4d4610
magnificodev <richardnguyen26th8@gmail.com>
2025-08-07 15:21:09 +0700

    Fix api url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-const API_URL = import.meta.VITE_API_BASE_URL || "/api"
+// const API_URL = import.meta.VITE_API_BASE_URL || "/api"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,14 +13,14 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
-    server: {
-        host: "0.0.0.0",
-        port: 3000,
-        proxy: API_URL.startsWith("/api") ? {
-            "/api": {
-                target: "http://localhost:8000",
-                changeOrigin: true,
-            },
-        } : undefined,
-    },
+    // server: {
+    //     host: "0.0.0.0",
+    //     port: 3000,
+    //     proxy: API_URL.startsWith("/api") ? {
+    //         "/api": {
+    //             target: "http://localhost:8000",
+    //             changeOrigin: true,
+    //         },
+    //     } : undefined,
+    // },
 })
