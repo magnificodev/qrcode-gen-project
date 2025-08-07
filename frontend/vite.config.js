a0d63c1571009fe3b503388cb96a66ad295e9b44
magnificodev <richardnguyen26th8@gmail.com>
2025-08-07 23:45:51 +0700

    Setup frontend Dockerfile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// const API_URL = import.meta.VITE_API_BASE_URL || "/api"
-
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
@@ -13,14 +11,12 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
-    // server: {
-    //     host: "0.0.0.0",
-    //     port: 3000,
-    //     proxy: API_URL.startsWith("/api") ? {
-    //         "/api": {
-    //             target: "http://localhost:8000",
-    //             changeOrigin: true,
-    //         },
-    //     } : undefined,
-    // },
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:8000",
+                changeOrigin: true,
+            },
+        }
+    },
 })
